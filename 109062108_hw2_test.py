76c44af0bedf59659c7471c7cddd9cc6059bfb36
1224K <kenchen901224@gmail.com>
2024-04-10 21:48:26 +0800

    4000

--- 109062108_hw2_test.py
+++ 109062108_hw2_test.py
@@ -9,8 +9,9 @@ import copy
 from collections import deque
 import random
 import cv2
-import matplotlib.pyplot as plt
-
+# import matplotlib.pyplot as plt
+import datetime
+from torchvision import transforms as T
 
 # Double Deep Q-Networks
 class DDQN(nn.Module):
@@ -48,75 +49,110 @@ class DDQN(nn.Module):
         
 class Agent(object):
     """DDQN """
-    def __init__(self, use_cuda=False, training=False):
+    def __init__(self):
         self.state_dim = (4, 84, 84)
         self.action_dim = 12
-        self.memory = deque(maxlen=40000)
-        self.batch_size = 64
+        self.memory = deque(maxlen=100000)
+        self.batch_size = 32
         
         self.net = DDQN(self.state_dim, self.action_dim)
         
-        self.use_cuda = use_cuda
-        self.training = training
-        if use_cuda:
-            self.net = self.net.cuda()
+        self.use_cuda = False
+        self.training = False
 
         self.exploration_rate = 0.1
         self.exploration_rate_decay = 0.9999975
-        self.exploration_rate_min = 0.01
+        self.exploration_rate_min = 0.1
         self.curr_step = 0
-
-        self.save_every = 5e5
 
         self.gamma = 0.9
         self.burnin = 1e4  # min. experiences before training
-        self.learn_every = 3  # no. of experiences between updates to Q_online
+        self.learn_every = 1  # no. of experiences between updates to Q_online
         self.sync_every = 1e4  # no. of experiences between Q_target & Q_online sync
-        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=0.00025)
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=0.0001) # 0.00025
         self.loss_fn = torch.nn.SmoothL1Loss()
 
-        self.frameStack = deque([], maxlen=4)
-        self.frameStack.extend([np.zeros((1, 84, 84), dtype=np.float32) for _ in range(4)])
+        self.frameStack = deque(maxlen=4)
+        self.action = 0
 
-        self.load("./model_3000")
+        self.init_state_hash = None
+        self.load("./model/model_4000")
+        # self.timestamp = datetime.datetime.now().strftime("%m%d_%H%M")
+        self.random_seed = 2
        
-    def act(self, state):
-        if np.random.rand() < self.exploration_rate:
-            action_idx =  np.random.choice(self.action_dim)
+    def act(self, observation):
+        if not self.training:
+            if self.init_state_hash is None:
+                self.init_state_hash = hash(observation.tobytes())
+                self.frameStack.extend([np.zeros((1, 84, 84), dtype=np.float32 ) for _ in range(4)])
+                self.curr_step = 0
+                self.action = 0
+                random.seed(self.random_seed)
+            elif self.init_state_hash == hash(observation.tobytes()):
+                self.frameStack.extend([np.zeros((1, 84, 84), dtype=np.float32) for _ in range(4)])
+                self.curr_step = 0
+                self.action = 0
+                random.seed(self.random_seed)
+
+            if self.curr_step % 4 != 0:
+                self.curr_step += 1
+                return self.action
+            
+        if random.random() < self.exploration_rate:
+            # self.action =  np.random.choice(self.action_dim)
+            self.action =  random.choice([1, 2])
+            if not self.training:
+                observation = np.transpose(observation, (2, 0, 1))
+                observation = torch.tensor(observation.copy(), dtype=torch.float)   
+                transform = T.Grayscale()
+                observation = transform(observation)
+                # ResizeObservation
+                transforms = T.Compose(
+                    [T.Resize((84, 84), antialias=True), T.Normalize(0, 255)]
+                )
+                observation = transforms(observation)
+               
+                # FrameStack
+                self.frameStack.append(observation)
         else :
             if not self.training:
                 # preporcessing the state
-                # ProcessFrame84
-                state = state[:, :, 0] * 0.299 + state[:, :, 1] * 0.587 + state[:, :, 2] * 0.114
-                resized_screen = cv2.resize(state, (84, 110), interpolation=cv2.INTER_AREA)
-                x_t = resized_screen[18:102, :]
-                x_t = np.reshape(x_t, [84, 84, 1])
-                # # draw the state
-                # plt.imshow(x_t, cmap='gray')
-                # plt.show()
-
-                # ImageToPyTorch
-                state = np.moveaxis(x_t, 2, 0)
-
+                # GrayScaleObservation
+                # permute [H, W, C] array to [C, H, W] tensor
+                observation = np.transpose(observation, (2, 0, 1))
+                observation = torch.tensor(observation.copy(), dtype=torch.float)   
+                transform = T.Grayscale()
+                observation = transform(observation)
+                # ResizeObservation
+                transforms = T.Compose(
+                    [T.Resize((84, 84), antialias=True), T.Normalize(0, 255)]
+                )
+                observation = transforms(observation)
+                # print(observation.shape)
+                # print(observation.numpy().shape)
+                # cv2.imwrite(f"./test/{self.curr_step}.png", observation.numpy()[0]*255)
                 # FrameStack
-                self.frameStack.append(state)
-                state = np.concatenate(self.frameStack, axis=0)
+                self.frameStack.append(observation)
 
+                observation = np.concatenate(self.frameStack, axis=0)
 
-            state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
+            observation = np.array(observation)
+            observation = torch.tensor(observation).unsqueeze(0)
 
             if self.use_cuda:
-                state = state.cuda()
-            q_values = self.net(state, model="online")
-            action_idx = torch.argmax(q_values, axis=1).item() 
+                observation = observation.cuda()
+            q_values = self.net(observation, model="online")
+            self.action = torch.argmax(q_values, axis=1).item()
 
         self.exploration_rate *= self.exploration_rate_decay
         self.exploration_rate = max(self.exploration_rate_min, self.exploration_rate) 
 
         self.curr_step += 1
-        return action_idx
+        return self.action
     
     def cache(self, state, next_state, action, reward, done):
+        state = np.array(state)
+        next_state = np.array(next_state)
         state = torch.tensor(state, dtype=torch.float)
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor([action])
@@ -135,7 +171,7 @@ class Agent(object):
     def recall(self):
         batch = random.sample(self.memory, self.batch_size)
         state, next_state, action, reward, done = map(torch.stack, zip(*batch))
-        return state, next_state, action, reward, done
+        return state, next_state, action.squeeze(), reward.squeeze(), done.squeeze()
     
     def td_estimate(self, state, action):
         current_Q = self.net(state, model="online")[
@@ -163,12 +199,12 @@ class Agent(object):
         self.net.target.load_state_dict(self.net.online.state_dict())
 
     def learn(self):
+        if self.curr_step % self.sync_every == 0:
+            self.sync_Q_target()
         if len(self.memory) < self.burnin:
             return None, None
         if self.curr_step % self.learn_every != 0:
             return None, None
-        if self.curr_step % self.sync_every == 0:
-            self.sync_Q_target()
         state, next_state, action, reward, done = self.recall()
         td_est = self.td_estimate(state, action)
         td_tgt = self.td_target(reward, next_state, done)
@@ -179,11 +215,9 @@ class Agent(object):
         torch.save(self.net.online.state_dict(), path)
     
     def load(self, path):
-        self.net.online.load_state_dict(torch.load(path))
-        self.net.target.load_state_dict(torch.load(path))
+        self.net.to(torch.device('cuda:0' if self.use_cuda else 'cpu'))
+        self.net.online.load_state_dict(torch.load(path, map_location=torch.device('cuda:0' if self.use_cuda else 'cpu')))
+        self.net.target.load_state_dict(torch.load(path, map_location=torch.device('cuda:0' if self.use_cuda else 'cpu')))
         for p in self.net.target.parameters():
             p.requires_grad = False
-
-    def to(self, device):
-        self.net.to(device)
-        return self
+    
